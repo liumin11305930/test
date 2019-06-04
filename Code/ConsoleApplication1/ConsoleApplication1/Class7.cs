04ff41cc0259f1d5269155dea61e9e6d6785c9da
liumin11305930 <11305930@qq.com>
2019-06-04 17:29:04 +0800

    3

--- Code/ConsoleApplication1/ConsoleApplication1/Class7.cs
+++ Code/ConsoleApplication1/ConsoleApplication1/Class7.cs
@@ -9,7 +9,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("11111");
+            Console.WriteLine("11111222");
         }
     }
 }
