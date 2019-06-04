7c5901f944e6b4c5cf63357aec0d54b745c0c189
LENOVO <LENOVO@DESKTOP-TQTSG8V>
2019-06-04 17:19:28 +0800

    1

---
+++ Code/ConsoleApplication1/ConsoleApplication1/Class7.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Class7
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("11111");
+        }
+    }
+}
